ec50c2aae288e4319e38ec033d9dfa8c6c463e07
Connor <dragonragers@gmail.com>
2019-04-27 21:31:27 -0400

    added experience difference

--- GameFrame.py
+++ GameFrame.py
@@ -7,6 +7,7 @@ class GameFrame:
         self.time = time #in game time in minutes
         self.baron = None #where 0 = no baron active, 1 = ally baron active, 2 = enemy baron active
         self.dragons = None #list dragon buffs, index is type and value is number of dragons taken. First 4 would be ally and later 4 would be enemy
+        self.xpDifference = None #difference in team expereience
         self.goldDifference = None #current team gold differentials
         self.turretDestroyed = None #TBD: either differential or tuple for number destroyed per team
         self.inhibsDestroyed = None #tuple for number each team has destoryed
