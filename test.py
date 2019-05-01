21d876dc7426b375a289cfcdec07a7bd574fd01d
Connor <dragonragers@gmail.com>
2019-05-01 08:13:07 -0400

    finding gameIds given a summoner name

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from gameData import generateData
 import time
 import csv
 
+queues = [400, 420, 440] #codes for relevent queues: https://developer.riotgames.com/game-constants.html
+
 def unpack(data):
     unpackedWins = []
     unpackedFrames = []
@@ -11,10 +13,11 @@ def unpack(data):
         unpackedFrames.append(frame)
     return unpackedWins, unpackedFrames
 
-def generateDataByGameIds(watcher, region, gameIds):
+def generateDataByGameIds(watcher, region, gameIds, queue = queues):
     wins = []
     frames = []
-    for gameId in gameIds:
+    for i, gameId in enumerate(gameIds):
+        print("Working on game", i, "of", len(gameIds))
         data = generateData(watcher, region, gameId)
         w, f = unpack(data)
         wins += w
@@ -30,19 +33,25 @@ def generateDataByGameIds(watcher, region, gameIds):
     print("File saved as:", fileName)
 
 
-def getGameIdBySummonerId(watcher, region, playerId):
-    matches = watcher.match.matchlist_by_account(region, playerId)
-    print(matches)
+def getGameIdsBySummonerName(watcher, region, name):
+    matchIds = []
+    id = watcher.summoner.by_name(region, name).get("accountId")
+    matches = watcher.match.matchlist_by_account(region, id).get("matches")
+    #print(matches)
+
+    for match in matches:
+        matchIds.append(match.get("gameId"))
+    return matchIds
+
 
 def main():
     key = input("Enter Riot API Key: ") #currently using development key, may apply for project key when project is actually working
     w = RiotWatcher(key)
     r = "na1"
-    """
-    g = [3016174568, 3019589039] #3024748419 #one of my recent games
+
+    g = getGameIdsBySummonerName(w, r, "DragonRagers") #3024748419 #one of my recent games
+    #g = g[:10]
     generateDataByGameIds(w, r, g)
-    """
-    getGameIdBySummonerId(w, r, )
 
 
 if __name__ == "__main__":
