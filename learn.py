8062ba3c1a75c3a22169db8e3b18621879d9ffb8
Connor <dragonragers@gmail.com>
2019-05-01 12:40:01 -0400

    tried somethings, isn't working so far

--- learn.py
+++ learn.py
@@ -4,30 +4,33 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 import numpy as np
 import csv
+import random
 
 file = open("data.csv", newline = "")
 reader = csv.reader(file,delimiter=",")
+next(reader) #skips the  coulum names
+
 
 wins = []
 frames = []
+data = []
 for row in reader:
-    wins.append(row[0])
-    frames.append(row[0:])
+    data.append(row)
+random.shuffle(data)
 
-"""
- x_train, y_train = data
-#x_train,y_train = makeData(False)
-#x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_train = x_train / 255
+for d in data:
+    wins.append(d[0])
+    frames.append(d[0:])
+x_train,y_train = np.array(frames), np.array(wins)
+
+
+#x_train = tf.keras.utils.normalize(x_train)
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(16 activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))
-#model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(13, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=300)
-model.save("model{}.model".format(time.time()))
-"""
+model.fit(x_train, y_train, epochs=50)
+#model.save("model{}.model".format(time.time()))
