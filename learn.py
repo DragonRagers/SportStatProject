11b4064b543f44ab3da70daaa07cee40acf3f911
Connor <dragonragers@gmail.com>
2019-05-02 14:12:32 -0400

    few changes to model shape

--- learn.py
+++ learn.py
@@ -1,10 +1,9 @@
 #from generateData import generateDataByGameIds, getGameIdsBySummonerName
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 import numpy as np
 import csv
 import random
+import time
 
 file = open("data.csv", newline = "")
 reader = csv.reader(file,delimiter=",")
@@ -20,7 +19,7 @@ random.shuffle(data)
 
 for d in data:
     wins.append(d[0])
-    frames.append(d[0:])
+    frames.append(d[1:])
 x_train,y_train = np.array(frames), np.array(wins)
 
 
@@ -29,8 +28,9 @@ x_train,y_train = np.array(frames), np.array(wins)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(1))
+model.add(tf.keras.layers.Activation("sigmoid"))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=50)
-#model.save("model{}.model".format(time.time()))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3)
+model.save("model{}.model".format(time.time()))
