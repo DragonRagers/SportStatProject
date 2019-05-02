a45d2ff7c1fbdf25efbddfc4769af5360b5f439c
DragonRagers <dragonragers@gmail.com>
2019-05-02 17:02:36 -0400

    added title, axis labels, and plots for red team

--- plot.py
+++ plot.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from riotwatcher import RiotWatcher
 from gameData import getGameFrames
 
-gameid = 3019589039
-watcher = RiotWatcher("RGAPI-bf4639fd-d553-4309-bbae-b87b43900c42")
+gameid = 3024748419
+watcher = RiotWatcher("RGAPI-ce1e9b86-88e7-4efb-8ff0-4aa1df56abda")
 data = np.array(getGameFrames(watcher, "na1", gameid))
 
 model = tf.keras.models.load_model("model.model")
@@ -13,9 +13,15 @@ model = tf.keras.models.load_model("model.model")
 predictions = model.predict(data)
 
 time = [item[0] for item in data]
-output = [item[0] for item in predictions]
+t1 = [item[0] for item in predictions]
+t2 = [1- item[0] for item in predictions]
 
 plt.ylim(0, 1)
-plt.plot(time, output, color = "blue")
+plt.xlabel("Time (minutes)")
+plt.ylabel("Chance of Winning (%)")
+plt.title("Chances of Winning vs Time")
+
 plt.plot(time, [.5]*len(time), color = "black")
+plt.plot(time, t1, color = "blue")
+plt.plot(time, t2, color = "red")
 plt.show()
