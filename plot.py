04345e271511d904279e159bd51bd0e21b3935f0
Connor <dragonragers@gmail.com>
2019-05-09 13:17:06 -0400

    put things into functions

--- plot.py
+++ plot.py
@@ -6,37 +6,48 @@ from gameData import getGameFrames
 import pickle
 from sklearn.linear_model import LogisticRegression
 
-gameid = 2988389854
-#key = input("Enter Riot API key:")
-key = "RGAPI-c0ac5943-5349-4396-9ac7-9d29a3458e09"
-watcher = RiotWatcher(key)
 
-data = getGameFrames(watcher, "na1", gameid)
-time = [item[0] for item in data]
+def plot(watcher, region, gameid, type = 0):
+    data = getGameFrames(watcher, region, gameid)
+    time = [item[0] for item in data]
 
-for i in range(len(data)):
-    data[i] = data[i][1:]
-data = np.array(data)
+    for i in range(len(data)):
+        data[i] = data[i][1:]
+    data = np.array(data)
 
-#data = np.array([[i] + [0]*19 for i in range(40)])
-if False: #neural net or logit model
-    model = tf.keras.models.load_model("model.model")
-    predictions = model.predict(data)
-    predictions = [p[0] for p in predictions]
-else:
-    logmodel = pickle.load(open("logmodel.p", "rb"))
-    predictions = logmodel.predict(data)
-    predictions = [int(i) for i in predictions]
+    #data = np.array([[i] + [0]*19 for i in range(40)])
+    if type == 0: #neural net or logit model
+        model = tf.keras.models.load_model("model.model")
+        predictions = model.predict(data)
+        predictions = [p[0] for p in predictions]
+    else:
+        logmodel = pickle.load(open("logmodel.p", "rb"))
+        predictions = logmodel.predict(data)
+        predictions = [int(i) for i in predictions]
 
-t1 = [item for item in predictions]
-t2 = [1-item for item in predictions]
+    t1 = [item for item in predictions]
+    t2 = [1-item for item in predictions]
 
-plt.ylim(0, 1)
-plt.xlabel("Time (minutes)")
-plt.ylabel("Chance of Winning (%)")
-plt.title("Chances of Winning vs Time")
+    plt.ylim(0, 1)
+    plt.xlabel("Time (minutes)")
+    plt.ylabel("Chance of Winning (%)")
+    plt.title("Chances of Winning vs Time")
 
-plt.plot(time, [.5]*len(time), color = "black")
-plt.plot(time, t1, color = "blue")
-plt.plot(time, t2, color = "red")
-plt.show()
+    plt.plot(time, [.5]*len(time), color = "black")
+    plt.plot(time, t1, color = "blue")
+    plt.plot(time, t2, color = "red")
+    plt.show()
+
+
+def main():
+
+    #key = input("Enter Riot API key:")
+    key = "RGAPI-8c6e3728-5b5d-45ff-8a82-07bf31d468ea"
+    w = RiotWatcher(key)
+    r = "na1"
+    g = 3036487185
+    plot(w,  r, g, 0)
+
+
+if __name__ == "__main__":
+    main()
