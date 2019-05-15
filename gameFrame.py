e37537b30b4ca2f8425ae12649df02fb65bd9bab
Connor <dragonragers@gmail.com>
2019-05-15 08:25:38 -0400

    changed gold and xp per minute to just gold and xp

--- gameFrame.py
+++ gameFrame.py
@@ -29,4 +29,5 @@ class GameFrame:
         self.inhibsDestroyed = [0,0]
 
     def toArray(self):
-        return [self.time] + self.baron + self.rift + self.dragons + [self.xpDifference / (self.time + 1), self.goldDifference / (self.time + 1), self.turretDifference] + self.inhibsDestroyed
+        return [self.time] + self.baron + self.rift + self.dragons + [self.xpDifference, self.goldDifference, self.turretDifference] + self.inhibsDestroyed
+        #return [self.time] + self.baron + self.rift + self.dragons + [self.xpDifference, self.goldDifference, self.turretDifference] + self.inhibsDestroyed
