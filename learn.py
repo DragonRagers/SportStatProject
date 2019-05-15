f78f2657f6fdc1d0903a42398babfdb455d15acd
Connor <dragonragers@gmail.com>
2019-05-15 08:59:19 -0400

    changed to make individual models per minute

--- learn.py
+++ learn.py
@@ -10,32 +10,43 @@ fileNames = []
 for fileName in os.listdir("data"):
     fileNames.append(fileName)
 #fileNames = ["tarzaned5", "sophist sage1", "pants are dragon", "santorin"]
-wins = []
-frames = []
+minuteData = []
 data = []
 
+#fileNames = ["tsm zven.csv"]
 for name in fileNames:
     file = open("data/{}".format(name), newline = "")
     reader = csv.reader(file,delimiter=",")
     for row in reader:
         data.append(row)
     file.close()
-random.shuffle(data)
 
 for d in data:
-    wins.append(d[0])
-    frames.append(d[2:]) #ignores who won and time
-x_train,y_train = np.array(frames), np.array(wins)
+    minute = int(d[1])
+    while minute > len(minuteData) - 1:
+        minuteData.append([])
 
 
-#x_train = tf.keras.utils.normalize(x_train)
+    minuteData[minute].append((d[0], d[2:]))
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(1))
-model.add(tf.keras.layers.Activation("sigmoid"))
+for i,md in enumerate(minuteData):
+    print(i, "minutes")
+    wins = []
+    frames = []
+    for f in md:
+        wins.append(f[0])
+        frames.append(f[1])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=2)
-model.save("model{}.model".format(time.time()))
+    x_train,y_train = np.array(frames), np.array(wins)
+
+    #x_train = tf.keras.utils.normalize(x_train)
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Activation("sigmoid"))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5)
+    model.save("nn/model{}.model".format(i))
